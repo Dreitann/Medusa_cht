f142de0390a2185754475a2113124573b581a5ac
Drei <Artem.nestarovich@gmail.com>
2025-12-25 14:53:08 +0300

    update

--- js/config.js
+++ js/config.js
@@ -3,7 +3,11 @@ export const SUPABASE_URL = 'https://edjfgdvdknewgfkmsail.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_J6SoPFzrip8i8pjQcM24Iw_N9tPzc_h';
 export const HW_BUCKET     = 'homework';
 export const VIDEO_BUCKET  = 'videos';
-
+const supabase = createClient(
+    "https://lwmervaxwrllgykcjfpn.supabase.co",
+    "PUBLIC_ANON_KEY"
+  );
+  
 // --- Google ---
 export const GOOGLE_CLIENT_ID = '156070127304-l8lqdcetr3rqdln2sfm59a1iu8is421l.apps.googleusercontent.com';
 export const GOOGLE_API_KEY   = 'AIzaSyALADqAQwTUzs1pLyI_ORMI8SULs5T0HN8';
